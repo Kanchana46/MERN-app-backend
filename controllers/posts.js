4e6bf372cd22282535c9c4dc5dec13de87afddc3
Kanchana46 <kanchana.rdg@gmail.com>
2022-08-18 14:19:20 +0530

    pagination and comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,9 +3,25 @@ import PostMessage from "../models/postMessages.js"
 
 export const getPosts = async (req, res) => {
     try {
-        const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages)
+        const { page } = req.query;
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT;
+        const total = await PostMessage.count({});
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
     } catch (error) {
+        console.log(error)
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await PostMessage.findById(id);
+        res.status(200).json(post);
+    } catch (error) {
+        console.log(error)
         res.status(404).json({ message: error.message });
     }
 }
@@ -65,3 +81,30 @@ export const likePost = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 }
+
+export const getPostsBySearch = async (req, res) => {
+    try {
+        const { searchQuery, tags } = req.query;
+        const title = new RegExp(searchQuery, 'i');
+        const posts = await PostMessage.find({ $or: [{ title }, { tags: { $in: tags.split(",") } }] });
+        const newArray = posts.map(data => data._id)
+        res.status(200).json({ data: posts });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const commentPost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { value } = req.body;
+        const post = await PostMessage.findById(id);
+        post.comments.push(value);
+        const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({ message: error.message });
+    }
+}
+
